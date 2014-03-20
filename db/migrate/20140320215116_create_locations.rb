185ab1c56389f9a2c360b56c5042790bcd2869d7
Apprentice <apprentice@dbc23.local>
2014-03-20 16:01:40 -0700

    Adds validation for isbn and databse restrictions.

--- db/migrate/20140320215116_create_locations.rb
+++ db/migrate/20140320215116_create_locations.rb
@@ -1,9 +1,9 @@
 class CreateLocations < ActiveRecord::Migration
   def change
     create_table :locations do |t|
-      t.string :city
-      t.string :state
-      t.string :zip
+      t.string :city, null: false
+      t.string :state, null: false
+      t.string :zip, null: false
     end
   end
 end
