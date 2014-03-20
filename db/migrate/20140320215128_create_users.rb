907351be691bb962aa0d7249b76b2542e27d03c9
Henry Hsu & Stephen M. Buchanan <pair+henryh28.papapoison@devbootcamp.com>
2014-03-20 15:58:23 -0700

    Adds user model/migration. Awaiting tests

--- db/migrate/20140320215128_create_users.rb
+++ db/migrate/20140320215128_create_users.rb
@@ -1,7 +1,11 @@
 class CreateUsers < ActiveRecord::Migration
-  def up
-  end
+  def change
+    create_table :users do |t|
+      t.string :username, null: false, unique: true
+      t.string :password_digest, null: false
+      t.string :email, null: false, unique: true
 
-  def down
+      t.timestamps
+    end
   end
 end
