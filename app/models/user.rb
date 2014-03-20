54f913fc5bbfb5a8a76b3717d6cf5c9f66f05cc5
Henry Hsu & Stephen M. Buchanan <pair+henryh28.papapoison@devbootcamp.com>
2014-03-20 16:08:59 -0700

    Fixes typos

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,7 +2,7 @@ class User < ActiveRecord::Base
   has_secure_password
 
   validates_presence_of :username, :password, :email
-  validates_uniqueness_of :username, :email, :case_sensative: false
+  validates_uniqueness_of :username, :email, :case_sensitive: false
 
   has_many :user_books
   has_many :books, through: :user_books
--- db/migrate/20140320215128_create_users.rb
+++ db/migrate/20140320215128_create_users.rb
@@ -4,8 +4,6 @@ class CreateUsers < ActiveRecord::Migration
       t.string :username, null: false, unique: true
       t.string :password_digest, null: false
       t.string :email, null: false, unique: true
-
-      t.timestamps
     end
   end
 end
