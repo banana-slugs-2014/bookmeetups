9e563846333c8b12b5f5c8048d933786d5959dbd
Apprentice <apprentice@dbc23.local>
2014-03-20 15:55:06 -0700

    Adds unit testing for book and location models.
    Adds factory cleaner gem to Gemfile.
    Adds validations to books and locations models.

--- app/models/book.rb
+++ app/models/book.rb
@@ -1,4 +1,6 @@
 class Book < ActiveRecord::Base
+  validates_presence_of :title
+  validates_presence_of :author
 
   has_many :user_books
   has_many :users, through :user_books
--- app/models/location.rb
+++ app/models/location.rb
@@ -1,4 +1,7 @@
 class Location < ActiveRecord::Base
+  validates_presence_of :city
+  validates_presence_of :state
+  validates_presence_of :zip
 
   has_many :user_locations
   has_many :users, through :user_locations
