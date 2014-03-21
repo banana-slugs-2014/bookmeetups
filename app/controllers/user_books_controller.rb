4c595995fc40f5f926292f90e759910f5f53b3a1
Apprentice <apprentice@dbc23.local>
2014-03-21 15:19:05 -0700

    Implements controller test and routes for userbook#create.
    Passes all tests.

--- app/controllers/user_books_controller.rb
+++ app/controllers/user_books_controller.rb
@@ -1,4 +1,8 @@
 class UserBooksController < ApplicationController
   def create
+    user = User.find(params[:user_id])
+    book = Book.find_or_create_by_isbn(params[:isbn])
+    user.books << book
+    redirect_to book_path(book)
   end
 end
--- spec/controllers/user_books_controller_spec.rb
+++ spec/controllers/user_books_controller_spec.rb
@@ -4,11 +4,21 @@ describe UserBooksController do
   let!(:my_user) { create :user }
   let!(:my_book) { create :book }
 
+
   context "#create" do
     it "should redirect" do
-      post :create, user_id: my_user, book_id: my_book, user_book: {}
+      post :create, user_id: my_user, isbn: my_book.isbn, user_book: {}
       expect(response).to be_redirect
     end
+
+    it "should increase count of UserBooks" do
+      expect{
+        post :create, user_id: my_user, isbn: my_book.isbn, user_book: {}
+      }.to change{ UserBook.count }.by(1)
+
+    end
+
+
   end
 
 end
