29ddf0a736275213af128b15bf41a652027c7417
Apprentice <apprentice@dbc23.local>
2014-03-21 12:33:12 -0700

    Fixes factory to create unique instances of user/book.
    Implements controller test for books index.
    Passes all tests.

--- spec/factories.rb
+++ spec/factories.rb
@@ -4,14 +4,14 @@ FactoryGirl.define do
     sequence(:username) { |n| Faker::Lorem.word + n.to_s }
     password "12345"
     password_confirmation "12345"
-    email Faker::Internet.email
+    email {Faker::Internet.email}
   end
 
   factory :book do
-    title Faker::Lorem.word
-    author Faker::Name.name
-    isbn Faker::Number.number(13)
-    google_id Faker::Number.number(15)
+    title {Faker::Lorem.word}
+    author {Faker::Name.name}
+    isbn {Faker::Number.number(13)}
+    google_id {Faker::Number.number(15)}
   end
-
 end
+
--- spec/features/book_spec.rb
+++ spec/features/book_spec.rb
@@ -1,13 +1,23 @@
 require 'spec_helper'
 
 describe "Books", :js => false do
-  let!(:book) { FactoryGirl.create :book }
+  let!(:my_book) { FactoryGirl.create(:book) }
+  let!(:book2) { FactoryGirl.create(:book) }
+  let!(:books) { [my_book, book2] }
 
-  it "shows a book's author and title" do
-    visit book_path(book)
-    expect(page).to have_content book.title
-    expect(page).to have_content book.author
+  it "a user can view a book's profile" do
+    visit book_path(my_book)
+    expect(page).to have_content my_book.title
+    expect(page).to have_content my_book.author
+  end
+
+  it "a user can view all books in the database" do
+    visit books_path
+    books.each do |book|
+      expect(page).to have_content book.title
+    end
   end
 end
+
 
 
