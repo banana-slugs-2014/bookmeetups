151efbbdf1cc39fd45fa46d3a62888b034033175
Apprentice <apprentice@dbc23.local>
2014-03-21 15:00:34 -0700

    Fixes underscore in routes.rb. One test fails in the right way.

--- config/routes.rb
+++ config/routes.rb
@@ -2,8 +2,9 @@ Bookmeetups::Application.routes.draw do
   root to: 'users#new'
   post "/search", to: "searches#search"
   resources :users do
-    post 'books/:book_id', to: 'userbooks#create'
-    delete 'books/:book_id', to: 'userbooks#destroy'
+    post 'books/:book_id', to: 'user_books#create'
+    delete 'books/:book_id', to: 'user_books#destroy'
+  end
 
   resources :books, only: [:create, :show, :index]
 
