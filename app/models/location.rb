f2c8f3fd916451a491d05563c997014878d1b369
Matthew Knudsen <mknudsen01@gmail.com>
2014-03-20 20:48:14 -0700

    Add geocoding to save latitude and longitude to Location
    * Add the geocoder gem
    * Add attr_accessible to city, state and zip
    * Add a method to make an 'address string'
    * Use the string as a query to geocoder to obtain the lat/lng

--- app/models/location.rb
+++ app/models/location.rb
@@ -1,7 +1,15 @@
 class Location < ActiveRecord::Base
+  attr_accessible :city, :state, :zip
   validates_presence_of :city
   validates_presence_of :state
   validates_presence_of :zip
 
   has_many :users
+  geocoded_by :address
+  after_validation :geocode
+
+
+  def address
+    [city, state, zip].compact.join(', ')
+  end
 end
