48bc16369b44163d061b6b43812c938200118a18
Matthew Knudsen <mknudsen01@gmail.com>
2014-03-22 01:05:28 -0700

    Add messages
    * Add the model, the migration, and some new tests for
      the association

--- app/models/meetup.rb
+++ app/models/meetup.rb
@@ -1,4 +1,5 @@
 class Meetup < ActiveRecord::Base
   has_many :user_meetups
   has_many :users, through: :user_meetups
+  has_many :messages
 end
--- spec/models/meetup_spec.rb
+++ spec/models/meetup_spec.rb
@@ -4,6 +4,7 @@ describe Meetup do
   context "associations" do
     it { should have_many(:user_meetups) }
     it { should have_many(:users).through(:user_meetups)}
+    it { should have_many(:messages) }
   end
 
 end
