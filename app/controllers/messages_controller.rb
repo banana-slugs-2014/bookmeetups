8c5220bb8d9517ef6fd633ec85fe0bb46e7634bf
Henry Hsu & Stephen M. Buchanan <pair+henryh28.papapoison@devbootcamp.com>
2014-03-23 12:34:46 -0700

    Adding tests for messages. Before filter.

--- app/controllers/messages_controller.rb
+++ app/controllers/messages_controller.rb
@@ -1,4 +1,6 @@
 class MessagesController < ApplicationController
+  before_filter :redirect_unless_logged_in
+
   def new
     @meetup = Meetup.where(id: params[:meetup_id]).first
     @message = Message.new
@@ -6,10 +8,12 @@ class MessagesController < ApplicationController
   end
 
   def create
+    p params
     meetup = Meetup.find(params[:meetup_id])
     message = Message.new
-    message.text = params[:text]
+    message.text = params[:message][:text]
     message.user = current_user
+    message.user_id = current_user.id
     if message.save
       meetup.messages << message
     end
--- spec/controllers/messages_controller_spec.rb
+++ spec/controllers/messages_controller_spec.rb
@@ -1,13 +1,30 @@
 require 'spec_helper'
 describe MessagesController do
-  let!(:meetup) { create :meetup }
-  let(:message){ create :message}
+  let!(:test_meetup) { create :meetup }
+  let(:test_message){ create :message}
   let!(:other_user) {create :user}
   let!(:current_user) { create :user }
+  before(:each) { request.session[:id] = current_user.id }
 
-  context "#create" do
-    it 'should be success'
-    it 'should add a message to the meetup'
+  context "#new" do
+    it 'should be success' do
+      get :new, { :meetup_id => test_meetup.id }
+      expect(response).to be_success
+    end
+
+    it 'should set a form for new message' do
+      get :new, { :meetup_id => test_meetup.id }
+      expect(assigns(:message)).to be_a_new(Message)
+    end
+  end
+
+  context '#create' do
+    let(:message_attribs) { attributes_for :message }
+
+    it "should be redirect" do
+      post :create, { :meetup_id => test_meetup.id, :message => message_attribs }
+      expect(response).to be_redirect
+    end
   end
 
 
