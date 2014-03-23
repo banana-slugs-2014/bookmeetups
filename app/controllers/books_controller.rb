e798d13b1e5b041c8ee2dd551e458df9e1974b18
Apprentice <apprentice@dbc23.local>
2014-03-22 17:08:45 -0700

    Fixes session issue with new user and root path.

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -7,11 +7,10 @@ class BooksController < ApplicationController
 
   def show
     @book = Book.find(params[:id])
-    render :partial => "show", locals: { book: @book, user_id: current_user.id }
+    render :partial => "show", locals: { book: @book }
   end
 
   def create
-    # p params[:users_books][:add]
   end
 
 end
