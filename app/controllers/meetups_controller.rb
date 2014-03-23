2d28f9f1177d6a047e3d02cc31fe61dd4912c325
Jamie & Matthew Knudsen <pair+jamck23.mknudsen01@devbootcamp.com>
2014-03-22 22:04:45 -0700

    WIP Trying to get meetups and messages views to work

--- app/controllers/meetups_controller.rb
+++ app/controllers/meetups_controller.rb
@@ -1,7 +1,16 @@
 class MeetupsController < ApplicationController
   def index
-    @meetups = current_user.meetups
-    render partial: "index", locals: { meetups: @meetups }
+    (redirect_to root_path && return) unless session[:id]
+    p "*" * 80
+    p session[:id]
+    p params[:user_id].to_i
+    p params[:user_id].to_i == session[:id]
+    if session[:id] == params[:user_id].to_i
+      @meetups = current_user.meetups
+      render :index
+    else
+      render partial: "shared/unauthorized"
+    end
   end
 
   def create
@@ -24,9 +33,13 @@ class MeetupsController < ApplicationController
     (redirect_to root_path && return) unless session[:id]
     meetup = Meetup.where(id: params[:id]).first
     current_user = User.where(id: params[:user_id]).first
-    #(redirect user_books_path(current_user) && return) unless meetup.users.include?(current_user)
-    @messages = meetup.messages
-    render :partial => 'show', :locals => {meetup: meetup}
+    (redirect_to user_books_path(current_user) && return) unless meetup.users.include?(current_user)
+    if current_user != params[:id]
+      @messages = meetup.messages
+      # render :partial => 'show', :locals => {meetup: meetup}
+    else
+      render :partial => 'shared/unauthorized'
+    end
   end
 
 end
