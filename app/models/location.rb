9a84d8f044f380b3ed9bd9968ad64ccc0828b87d
hpchess <hpchess@gmail.com>
2014-03-23 14:01:25 -0700

    Change db seed to add new users and 3 seperate locations. Change lat_long_distance converter and location to calculate distance properly after the aproximation. Add working user friends(book, distance) method to find freinds for a particular book. Add spec file to start testing these features.

--- app/models/location.rb
+++ app/models/location.rb
@@ -13,19 +13,26 @@ class Location < ActiveRecord::Base
     [city, state, zip].compact.join(', ')
   end
 
-  def self.locations_within_range(lat, long, miles)
+  def self.find_limits(lat, long, miles)
     fla, cla =LatLongDistanceConverter.lat_range(lat, miles) 
     flo, clo =LatLongDistanceConverter.long_range(lat, long, miles) 
-    limits = {
-      floorlat: fla,
-      ceillat: cla,
-      floorlong: flo,
-      ceillong: clo
-    }
-    floorlat, ceillat = LatLongDistanceConverter.lat_range(lat, miles)
-    floorlong, ceillong = LatLongDistanceConverter.long_range(lat, long, miles) 
-    self.where("latitude >= :floorlat AND latitude <= :ceillat AND longitude >= :floorlong AND longitude <= :ceillong",
-      limits)
+    { floorlat: fla, ceillat: cla,
+      floorlong: flo, ceillong: clo}
+  end
+
+  def self.locations_within_range(lat, long, miles)
+    limits = Location.find_limits(lat, long, miles)
+    locations = self.where("latitude >= :floorlat AND latitude <= :ceillat AND longitude >= :floorlong AND longitude <= :ceillong", limits)
+    Location.locations_within_range_circle(lat, long, miles, locations)
+  end
+
+  def self.locations_within_range_circle(lat, long, miles, locations)
+    in_range = []
+    locations.each do |location| 
+      d = LatLongDistanceConverter.distance_between_two_points(location.latitude, lat, location.longitude, long)
+      in_range << location if d <= miles
+    end
+    in_range
   end
 
   def in_range( miles )
