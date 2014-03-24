a577184464e9829badc14f156be91c957900783d
Henry Hsu & Stephen M. Buchanan <pair+henryh28.papapoison@devbootcamp.com>
2014-03-23 19:41:31 -0700

    Adds error for login

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -6,7 +6,8 @@ class ApplicationController < ActionController::Base
   end
 
   def redirect_unless_logged_in
-    redirect_to(root_path) unless logged_in?
+    redirect_to(root_path, :flash =>
+      {:error => "You must be logged in to do that"}) unless logged_in?
   end
 
   def current_user
