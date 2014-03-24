678d604f5abdffe47cade57b4bc0b15e3d55401a
Matthew Knudsen <mknudsen01@gmail.com>
2014-03-24 10:57:43 -0700

    Ajax-ify the messages
    * Currently separated out to MVC.
    * Would be good to add selectors as an object literal to throw in
      the view and controller

--- app/assets/javascripts/bookmeetups.js
+++ app/assets/javascripts/bookmeetups.js
@@ -1,39 +1,51 @@
 var Bookmeetups = Bookmeetups || {};
 
-
 Bookmeetups.Controller = function(view){
   this.view = view;
 };
 
 Bookmeetups.Controller.prototype = {
   showNewMessage: function(form){
-    this.view.displayNewMessageForm(form);
-    this.view.hideCreateMessageLink();
+    this.view.renderNewMessageForm(form);
   },
 
   showCreatedMessageEvent: function(newMessage){
-    this.view.prependNewMessage(newMessage);
-
-    $('form').remove();
-    $('.open-new-message').show();
+    this.view.renderNewMessage(newMessage);
   }
 };
 
-Bookmeetups.View = function(controller){
-  this.controller = controller;
-};
+Bookmeetups.View = function(){};
 
-Bookmeetups.View. prototype = {
-  displayNewMessageForm: function(formHTML){
-    $('.messages').prepend(formHTML);
+Bookmeetups.View.prototype = {
+  renderNewMessageForm: function(form){
+    this.displayNewMessageForm(form);
+    this.hideCreateMessageLink();
+  },
+
+  displayNewMessageForm: function(form){
+    $('.messages').prepend(form);
   },
 
   hideCreateMessageLink: function(){
     $('.open-new-message').hide();
   },
 
+  renderNewMessage: function(newMessage){
+    this.prependNewMessage(newMessage);
+    this.removeNewMessageForm();
+    this.showCreateMessageLink();
+  },
+
   prependNewMessage: function(newMessage){
     $('.messages').prepend(newMessage);
+  },
+
+  removeNewMessageForm: function(){
+    $('#new_message').remove();
+  },
+
+  showCreateMessageLink: function(){
+    $('.open-new-message').show();
   }
 };
 
@@ -55,18 +67,15 @@ Bookmeetups.Binder.prototype = {
   },
 
   showCreatedMessage: function(){
-    var self = this
+    var self = this;
     $('body').on("ajax:success", '#new_message', function(event, newMessage){
       self.controller.showCreatedMessageEvent(newMessage);
     });
   }
 };
 
-
 $(document).ready(function() {
-
-  Bookmeetups.view = new Bookmeetups.View;
+  Bookmeetups.view = new Bookmeetups.View();
   Bookmeetups.controller = new Bookmeetups.Controller(Bookmeetups.view);
-
   new Bookmeetups.Binder(Bookmeetups.controller).bind();
 });
