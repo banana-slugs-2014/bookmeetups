3d7c80479e9941477ff008842abc232730ded089
Matthew Knudsen <mknudsen01@gmail.com>
2014-03-24 10:11:41 -0700

    Toggle link to open form
    * Disappears when form is open
    * Reappears when form is sent

--- app/assets/javascripts/bookmeetups.js
+++ app/assets/javascripts/bookmeetups.js
@@ -3,9 +3,11 @@ var Bookmeetups = Bookmeetups || {};
 $(document).ready(function() {
   $('body').on("ajax:success", '.open-new-message', function(event,response){
     $('.messages').prepend(response);
+    $('.open-new-message').hide();
   });
   $('body').on("ajax:success", '#new_message', function(event, response){
     $('.messages').prepend(response);
     $('form').remove();
-  })
+    $('.open-new-message').show();
+  });
 });
