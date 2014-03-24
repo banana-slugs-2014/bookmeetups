f0a093dd93b8e172d2da93df24439b517dc427c5
Henry Hsu & Stephen M. Buchanan <pair+henryh28.papapoison@devbootcamp.com>
2014-03-23 19:47:39 -0700

    User edit and photo create have html 5 validations now

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -7,7 +7,8 @@ class ApplicationController < ActionController::Base
 
   def redirect_unless_logged_in
     redirect_to(root_path, :flash =>
-      {:error => "You must be logged in to do that"}) unless logged_in?
+      {:error => "You must be logged in to do that"}
+      ) unless logged_in?
   end
 
   def current_user
