34103a54665fe85f771b91ebf2777267fea1b3f4
Apprentice <apprentice@dbc23.local>
2014-03-24 19:08:26 -0700

    Removed inappropriate validation message

--- app/models/favorite_book.rb
+++ app/models/favorite_book.rb
@@ -2,5 +2,5 @@
   belongs_to :user
   belongs_to :book
 
-  validates_uniqueness_of :user_id, :scope => :book_id, :msg => "No dupes yo"
+  validates_uniqueness_of :user_id, :scope => :book_id, :msg => "No duplicates entries allowed"
 end
