cd062793d4f46a8e862a7b7cd0feeca0970aa121
Apprentice <apprentice@dbc23.local>
2014-03-25 16:05:28 -0700

    Fixes subtle search bug
    Adjusts migration to use 'text' as opposed to 'string.'
    This removes a search error caused by the length of title or author.

--- app/models/book.rb
+++ app/models/book.rb
@@ -2,6 +2,7 @@ class Book < ActiveRecord::Base
   validates_presence_of :title
   validates_presence_of :author
   validates_presence_of :isbn
+  validates_length_of :title, :maximum => 255
   validates_uniqueness_of :isbn
   validates_uniqueness_of :google_id
   validates_uniqueness_of :author, :scope => :title
@@ -11,8 +12,15 @@ class Book < ActiveRecord::Base
   has_many :favorite_books
   has_many :users, through: :favorite_books
   has_many :meetups
+  before_save :check_title_length
 
   def photo_link( zoom = 2)
     "http://bks7.books.google.com/books?id=#{google_id}&printsec=frontcover&img=1&zoom=#{zoom}"
+  end
+
+  private
+  def check_title_length
+    return false if title.length >= 255
+    true
   end
 end
--- db/migrate/20140320215054_create_books.rb
+++ db/migrate/20140320215054_create_books.rb
@@ -1,8 +1,8 @@
 class CreateBooks < ActiveRecord::Migration
   def change
     create_table :books do |t|
-      t.string :title, null: false
-      t.string :author, null: false
+      t.text :title, null: false
+      t.text :author, null: false
       t.string :isbn, null: false, unique: true
       t.string :google_id, unique: true
     end
