432e271ebdf4a185c1630a478912a06e12172bc9
hpchess <hpchess@gmail.com>
2014-03-25 10:28:18 -0700

    increase the number of search results to get on a book search. increase the number of books needed to not search remotly

--- app/models/searcher.rb
+++ app/models/searcher.rb
@@ -2,8 +2,8 @@ class Searcher
   def self.search(terms)
     terms = "" if terms==nil
     local = BookSearch.search( terms )
-    if local.length < 5
-      return local + GoogleSearch.search_and_add( terms, {:count => 10})
+    if local.length < 15
+      return local + GoogleSearch.search_and_add( terms, {:count => 20})
     end
     return local.uniq
   end
