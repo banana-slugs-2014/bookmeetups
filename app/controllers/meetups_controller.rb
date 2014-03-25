6b0f4edd6861854f8ce16ad6175e0c33fa4b76c5
Matthew Knudsen <mknudsen01@gmail.com>
2014-03-24 23:24:20 -0700

    Make it look nicer
    * Add a carriage return on the long if statement
    * remove an unnecessary if nil?

--- app/controllers/meetups_controller.rb
+++ app/controllers/meetups_controller.rb
@@ -11,10 +11,11 @@ class MeetupsController < ApplicationController
     @other_user = User.find(params[:user_id])
     book = Book.find(params[:book_id])
     book.meetups.each do |existing_meetup|
-      @meetup = existing_meetup if (existing_meetup.users.include?(current_user ) && existing_meetup.users.include?(User.find(params[:user_id])))
+      @meetup = existing_meetup if (existing_meetup.users.include?(current_user ) &&
+                                    existing_meetup.users.include?(User.find(params[:user_id])))
     end
     if @meetup.nil?
-      @meetup = Meetup.create if @meetup.nil?
+      @meetup = Meetup.create
       book.meetups << @meetup
       current_user.meetups << @meetup
       @other_user.meetups << @meetup
