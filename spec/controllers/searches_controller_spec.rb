1339aab265a100e5defbbc7599a2e1f0981d0040
Stephen M. Buchanan <buchanan.stephenm@gmail.com>
2014-03-26 12:29:44 -0700

    Changes formatting for consistancy

--- spec/controllers/searches_controller_spec.rb
+++ spec/controllers/searches_controller_spec.rb
@@ -3,12 +3,14 @@ describe SearchesController do
   let!(:book) {FactoryGirl.create :book}
   before(:each) { session[:id] = 1 }
 
-  context "results page" do
+  context '#search' do
     it "redirects when posed to" do
       post :search, search: "bob"
       expect( response ).to be_redirect
     end
+  end
 
+  context '#results' do
     it "finds the searched term in our database" do
       GoogleSearch.stub(:search_and_add) { [] }
       get :results,  :s => book.title
