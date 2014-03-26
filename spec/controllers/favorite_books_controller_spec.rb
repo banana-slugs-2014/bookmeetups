5a1a999823d4a4ab6e678d4fb32c95e1f92cdd8e
Apprentice <apprentice@dbc23.local>
2014-03-25 18:40:57 -0700

    Adds/removes pending tests for two controllers

--- spec/controllers/favorite_books_controller_spec.rb
+++ spec/controllers/favorite_books_controller_spec.rb
@@ -10,15 +10,54 @@ describe FavoriteBooksController do
   context "#create" do
     let!(:my_book2) { create :book }
 
-    it "should redirect" do
-      post :create, user_id: my_user, book_id: my_book2, favorite_book: {}
-      expect(response).to be_redirect
+    context 'with valid params' do
+      it "should redirect" do
+        post :create, user_id: my_user, book_id: my_book2, favorite_book: {}
+        expect(response).to be_redirect
+      end
+
+      it "should increase count of FavoriteBooks" do
+        expect{
+          post :create, user_id: my_user, book_id: my_book2, favorite_book: {}
+        }.to change{ FavoriteBook.count }.by(1)
+      end
     end
 
-    it "should increase count of FavoriteBooks" do
-      expect{
-        post :create, user_id: my_user, book_id: my_book2, favorite_book: {}
-      }.to change{ FavoriteBook.count }.by(1)
+    context 'with a previously favorited book' do
+      it "should be redirect" do
+        post :create, user_id: my_user, book_id: my_book, favorite_book: {}
+        expect(response).to be_redirect
+      end
+
+      it "should not increase FavoriteBook count" do
+        expect {
+          post :create, user_id: my_user, book_id: my_book, favorite_book: {}
+          }.to_not change { FavoriteBook.count }
+      end
+    end
+
+    context 'with missing params' do
+      it "should be an error" do
+        expect {
+          post :create, user_id: my_user, book_id: nil, favorite_book: {}
+        }.to raise_error(ActionController::RoutingError,
+          'No route matches {:user_id=>"1", :book_id=>nil, :favorite_book=>{}, :controller=>"favorite_books", :action=>"create"}')
+      end
+    end
+
+    context 'when not logged in' do
+      before(:each) { request.session[:id] = nil }
+
+      it "should be redirect" do
+        post :create, user_id: my_user, book_id: my_book, favorite_book: {}
+        expect(response).to be_redirect
+      end
+
+      it "should not change FavoriteBook count" do
+        expect{
+          post :create, user_id: my_user, book_id: my_book2, favorite_book: {}
+        }.to_not change{ FavoriteBook.count }
+      end
     end
   end
 
@@ -39,6 +78,11 @@ describe FavoriteBooksController do
     it "should be ok" do
       get :index, user_id: my_user
       expect(response).to be_success
+    end
+
+    it "should find the correct user" do
+      get :index, user_id: my_user
+      expect(assigns(:user)).to eq(my_user)
     end
   end
 end
