897b01163cd8ccbbcce848c47601fdf6e60fa4d4
Apprentice <apprentice@dbc23.local>
2014-03-26 17:30:01 -0700

    Removes duplicate logic from from ApplicationHelper

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,13 +1,5 @@
 module ApplicationHelper
 
-  def logged_in?
-    !!session[:id]
-  end
-
-  def current_user
-    User.find(session[:id]) if session[:id]
-  end
-
   def first_button_text
     if logged_in?
       "Log Out"
